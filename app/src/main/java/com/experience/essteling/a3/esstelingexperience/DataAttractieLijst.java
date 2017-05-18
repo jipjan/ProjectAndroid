3003949af128149ce9977cff5cf7475fc5d0c293
Renske <rrveen@avans.nl>
2017-05-18 15:40:12 +0200

    layout finished

--- app/src/main/java/com/experience/essteling/a3/esstelingexperience/DataAttractieLijst.java
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/DataAttractieLijst.java
@@ -1,6 +1,5 @@
 package com.experience.essteling.a3.esstelingexperience;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +12,7 @@ public class DataAttractieLijst extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_attractie_lijst);
 
-        btn_metingen_achtbaan_menu = (Button) findViewById(R.id.btn_metingen_achtbaan_menu);
+        btn_metingen_achtbaan_menu = (Button) findViewById(R.id.btn_data_attractie_lijst_menu);
         btn_metingen_achtbaan_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

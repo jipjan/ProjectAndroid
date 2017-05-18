224d07039e210329daa572bd4656644a6abf6328
Renske <rrveen@avans.nl>
2017-05-18 09:20:17 +0200

    made most of templates

--- app/src/main/java/com/experience/essteling/a3/esstelingexperience/Meeting.java
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/Meeting.java
@@ -3,11 +3,11 @@ package com.experience.essteling.a3.esstelingexperience;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Meeting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_meeting);
     }
 }
---
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/Metingen.java
@@ -0,0 +1,13 @@
+package com.experience.essteling.a3.esstelingexperience;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Metingen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metingen);
+    }
+}

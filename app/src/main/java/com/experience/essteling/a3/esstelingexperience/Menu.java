91c53e01b08270ee9ba67f83fed77b0ed68b4090
Renske <rrveen@avans.nl>
2017-05-18 12:35:55 +0200

    templates soort van af

--- app/src/main/java/com/experience/essteling/a3/esstelingexperience/Menu.java
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/Menu.java
@@ -1,13 +1,35 @@
 package com.experience.essteling.a3.esstelingexperience;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
-
+    public Button btn_menu_meetmij;
+    public Button btn_menu_metingen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Menu);
+        setContentView(R.layout.activity_menu);
+
+        btn_menu_meetmij = (Button) findViewById(R.id.btn_menu_meetmij);
+        btn_menu_meetmij.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MeetMij.class);
+                startActivity(i);
+            }
+        });
+
+        btn_menu_metingen = (Button) findViewById(R.id.btn_menu_metingen);
+        btn_menu_metingen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Metingen.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/experience/essteling/a3/esstelingexperience/Metingen.java
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/Metingen.java
@@ -1,13 +1,25 @@
 package com.experience.essteling.a3.esstelingexperience;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Metingen extends AppCompatActivity {
-
+    public Button btn_metingen_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metingen);
+
+        btn_metingen_menu= (Button)findViewById(R.id.btn_metingen_menu);
+        btn_metingen_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Menu.class);
+                startActivity(i);
+            }
+        });
     }
 }

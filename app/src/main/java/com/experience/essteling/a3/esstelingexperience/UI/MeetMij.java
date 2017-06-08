5178db3104549b66512c534b45f3cd499e9b62c6
Bram Stoof <bramstoof@live.com>
2017-06-08 13:52:42 +0200

    animatie toegevoegd aan meet mij scherm

--- app/src/main/java/com/experience/essteling/a3/esstelingexperience/UI/MeetMij.java
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/UI/MeetMij.java
@@ -1,12 +1,15 @@
 package com.experience.essteling.a3.esstelingexperience.UI;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -23,12 +26,35 @@ public class MeetMij extends AppCompatActivity {
     public Button btn_meet_mij_stop;
     public ProgressBar pb_meet_mij;
     public Button btn_meet_mij_start;
+    public ImageView imageBackground1;
+    public ImageView imageBackground2;
+    public ImageView imageBackground3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meet_mij);
         final Attractie attractie = (Attractie) getIntent().getSerializableExtra("ATTRACTIE");
+
+        imageBackground1 = (ImageView) findViewById(R.id.background1_meetmij);
+        imageBackground2 = (ImageView) findViewById(R.id.background2_meetmij);
+        imageBackground3 = (ImageView) findViewById(R.id.background3_meetmij);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = imageBackground1.getWidth() + 200;
+                final float translationX = width * progress;
+                imageBackground1.setTranslationX(translationX);
+                imageBackground2.setTranslationX(translationX - width);
+            }
+        });
 
 
         setTitle("Meet mij");
@@ -61,7 +87,7 @@ public class MeetMij extends AppCompatActivity {
             public void onClick(View v) {
                 Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
                 vibrator.vibrate(1000); // 5000 miliseconds = 5 seconds
-
+                animator.start();
                 WifiConnection.Connect(getApplicationContext());
 
                 while (!WifiConnection.isConnected(getApplication())) {

0ea364ea52e4c060be19cbe02558ea762be30a88
DESKTOP-9902CT1\Bram PC <bfwstoof@avans.nl>
2017-06-16 14:06:37 +0200

    animatie toegevoegd bij metingenlijst

--- app/src/main/java/com/experience/essteling/a3/esstelingexperience/UI/MetingenLijst.java
+++ app/src/main/java/com/experience/essteling/a3/esstelingexperience/UI/MetingenLijst.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.experience.essteling.a3.esstelingexperience.Adapters.LijstAdapterMetingen;
 import com.experience.essteling.a3.esstelingexperience.Entities.Attractie;
@@ -14,10 +17,14 @@ import com.experience.essteling.a3.esstelingexperience.R;
 
 import java.util.ArrayList;
 
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
 public class MetingenLijst extends AppCompatActivity {
     RecyclerView lv_attracties;
     ArrayList<Attractie> attracties = new ArrayList<>();
     public static View.OnClickListener Click;
+    TextView geen_metingen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +37,13 @@ public class MetingenLijst extends AppCompatActivity {
             public void onClick(View v) {
                 final int position = lv_attracties.getChildLayoutPosition(v);
                 int aantal = MetingenData.ITEMS.getListOrNew(attracties.get(position).getId()).size();
-                if(aantal == 0){
-
-                }else {
+                if (aantal == 0) {
+                    geen_metingen = (TextView) findViewById(R.id.geen_metingen);
+                    geen_metingen.setVisibility(VISIBLE);
+                    Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.resize);
+                    geen_metingen.startAnimation(animation);
+                    geen_metingen.setVisibility(INVISIBLE);
+                } else {
                     Attractie attractie = attracties.get(position);
                     Intent intent = new Intent(getApplicationContext(), DataAttractieLijst.class);
                     intent.putExtra("ATTRACTIE", attractie);
@@ -51,26 +62,26 @@ public class MetingenLijst extends AppCompatActivity {
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         lv_attracties.setLayoutManager(llm);
 
-        attracties.add(new Attractie("4D-film",R.drawable.movie, "Show",1));
-        attracties.add(new Attractie("Bobslee",R.drawable.bobslee, "Attractie",2));
-        attracties.add(new Attractie("Doolhof",R.drawable.doolhof, "Kinder attractie",3));
-        attracties.add(new Attractie("Botsauto's",R.drawable.botsautos,"Kinder attractie",4));
-        attracties.add(new Attractie("Rupsje blij",R.drawable.rupsje,"Kinder attractie",5));
-        attracties.add(new Attractie("Spookhuis",R.drawable.spookhuis,"Kinder attractie",6));
-        attracties.add(new Attractie("Klimmuur",R.drawable.klimmuur,"Kinder attractie",7));
-        attracties.add(new Attractie("Uitkijktoren",R.drawable.uitkijktoren, "Attractie",8));
-        attracties.add(new Attractie("Zweefmolen",R.drawable.zweefmolen, "Attractie",9));
-        attracties.add(new Attractie("Megatron",R.drawable.achtbaana,"Attractie",10));
-        attracties.add(new Attractie("Mini trein",R.drawable.mini_trein,"Kinder attractie",11));
-        attracties.add(new Attractie("Silver rush",R.drawable.silver,"Attractie",12));
-        attracties.add(new Attractie("Reuzenrad",R.drawable.reuzenras, "Attractie",13));
-        attracties.add(new Attractie("You Pirate!",R.drawable.piraat, "Attractie",14));
-        attracties.add(new Attractie("Cobra",R.drawable.cobr, "Attractie",15));
-        attracties.add(new Attractie("Kabouterrit",R.drawable.kabouter,"Kinder attractie",16));
-        attracties.add(new Attractie("Treintje",R.drawable.treintjes, "Kinder attractie", 17));
-        attracties.add(new Attractie("Wilde leeuw",R.drawable.leeuw, "Attractie", 18));
-        attracties.add(new Attractie("Jungle",R.drawable.jungle,"Attractie",19));
-        attracties.add(new Attractie("the Force", R.drawable.achtbaan,"Attractie",20));
+        attracties.add(new Attractie("4D-film", R.drawable.movie, "Show", 1));
+        attracties.add(new Attractie("Bobslee", R.drawable.bobslee, "Attractie", 2));
+        attracties.add(new Attractie("Doolhof", R.drawable.doolhof, "Kinder attractie", 3));
+        attracties.add(new Attractie("Botsauto's", R.drawable.botsautos, "Kinder attractie", 4));
+        attracties.add(new Attractie("Rupsje blij", R.drawable.rupsje, "Kinder attractie", 5));
+        attracties.add(new Attractie("Spookhuis", R.drawable.spookhuis, "Kinder attractie", 6));
+        attracties.add(new Attractie("Klimmuur", R.drawable.klimmuur, "Kinder attractie", 7));
+        attracties.add(new Attractie("Uitkijktoren", R.drawable.uitkijktoren, "Attractie", 8));
+        attracties.add(new Attractie("Zweefmolen", R.drawable.zweefmolen, "Attractie", 9));
+        attracties.add(new Attractie("Megatron", R.drawable.achtbaana, "Attractie", 10));
+        attracties.add(new Attractie("Mini trein", R.drawable.mini_trein, "Kinder attractie", 11));
+        attracties.add(new Attractie("Silver rush", R.drawable.silver, "Attractie", 12));
+        attracties.add(new Attractie("Reuzenrad", R.drawable.reuzenras, "Attractie", 13));
+        attracties.add(new Attractie("You Pirate!", R.drawable.piraat, "Attractie", 14));
+        attracties.add(new Attractie("Cobra", R.drawable.cobr, "Attractie", 15));
+        attracties.add(new Attractie("Kabouterrit", R.drawable.kabouter, "Kinder attractie", 16));
+        attracties.add(new Attractie("Treintje", R.drawable.treintjes, "Kinder attractie", 17));
+        attracties.add(new Attractie("Wilde leeuw", R.drawable.leeuw, "Attractie", 18));
+        attracties.add(new Attractie("Jungle", R.drawable.jungle, "Attractie", 19));
+        attracties.add(new Attractie("the Force", R.drawable.achtbaan, "Attractie", 20));
 
 
         lv_attracties.setAdapter(new LijstAdapterMetingen(attracties));
